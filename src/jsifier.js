a15b8308696d426931aa2834188703191d4070e7
Alon Zakai <alonzakai@gmail.com>
2013-04-22 11:17:00 -0700

    fully align varargs and formatString arguments all to 64-bit

--- src/jsifier.js
+++ src/jsifier.js
@@ -1368,6 +1368,7 @@ function JSify(data, functionsOnly, givenFunctions) {
         } else {
           size = Runtime.getNativeFieldSize(param.type);
         }
+        size = Runtime.alignMemory(size, Runtime.STACK_ALIGN);
         varargs.push(val);
         varargs = varargs.concat(zeros(size-1));
         // TODO: replace concats like this with push
